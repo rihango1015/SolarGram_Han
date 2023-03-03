b245282b6d2de7dd538c78f8a9546e0bc35c06c8
rihango1015 <hgao7461@usc.edu>
2023-03-02 17:04:06 -0800

    Initial Commit

---
+++ SolarGram_Han/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SolarGram_Han
+//
+//  Created by Han Gao on 3/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SolarGram_Han/SolarGram_HanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarGram_HanApp.swift
+//  SolarGram_Han
+//
+//  Created by Han Gao on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SolarGram_HanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
