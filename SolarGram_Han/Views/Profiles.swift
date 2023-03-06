13a30e46ed33918c894d859f8ea40b468f816a61
rihango1015 <hgao7461@usc.edu>
2023-03-06 00:38:55 -0800

    Update Profiles.swift

--- SolarGram_Han/Views/Profiles.swift
+++ SolarGram_Han/Views/Profiles.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct Profiles: View {
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 3, alignment: nil),
+        GridItem(.flexible(), spacing: 3, alignment: nil),
+        GridItem(.flexible(), spacing: 3, alignment: nil),
+    ]
+    
     var body: some View {
-        ZStack {
-            Color.black
-            Image(systemName: "person.crop.circle.fill")
-                .foregroundColor(Color.white)
-                .font(.system(size: 100))
-            
+        
+    ScrollView {
+        VStack {
+            Image("person_1")
+                .resizable()
+                .frame(width: 80, height: 80)
+                .cornerRadius(80)
+                .overlay(
+                Circle().stroke(Color.gray, lineWidth: 0.2)
+                    )
+                Text("Rihango")
+                
+                LazyVGrid(
+                    columns: columns,
+                    alignment: .center,
+                    spacing: 1,
+                    pinnedViews: [],
+                    content: {
+                        ForEach(0..<9) {
+                            index in Rectangle()
+                                .frame(height: 120)
+                        }
+                })
+            }
         }
     }
 }
