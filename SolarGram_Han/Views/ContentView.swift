5445a25ac9063f482576bbe4adcafc2ed974de22
rihango1015 <hgao7461@usc.edu>
2023-03-26 22:43:14 -0700

    Update ContentView.swift

--- SolarGram_Han/Views/ContentView.swift
+++ SolarGram_Han/Views/ContentView.swift
@@ -9,32 +9,69 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @EnvironmentObject var viewModel: PublicPostsViewModel
+    
     @State private var selection: Tab = .photos
+    @State private var showCameraPicker = false
+    @State private var showImagePicker = false
     
     enum Tab {
-           case photos
-           case profile
-       }
+        case photos
+        case profile
+    }
     
     var body: some View {
-        TabView(selection: $selection) {
-            PhotoList()
-                .tabItem {
-                    Label("Photos", systemImage: "photo.stack")
-                }
-                .tag(Tab.photos)
-            ProfileView()
-                .tabItem {
-                    Label("Profile", systemImage: "person.crop.circle")
-                }
-                .tag(Tab.profile)
+        
+        VStack{
             
+            // Add an HStack that includes a "SolarGram" title, "Camera", and "Image" icons, mimicking Instagram's layout
+            
+            HStack{
+                Text("SolarGram")
+                    .font(Font.custom("American Typewriter", size: 24))
+                    .fontWeight(.bold)
+                Spacer()
+                Button {
+                    self.showCameraPicker = true
+                } label: {
+                    Image(systemName: "camera")
+                }
+                .sheet(isPresented: $showCameraPicker) {
+                    CameraPicker(viewModel: viewModel)
+                }
+                .padding(.horizontal, 10)
+                Button {
+                    self.showImagePicker = true
+                } label: {
+                    Image(systemName: "photo")
+                }
+                .sheet(isPresented: $showImagePicker) {
+                    ImagePicker(viewModel: viewModel)
+                    
+                }
+            }
+            .padding(.horizontal, 10)
+            
+            TabView(selection: $selection) {
+                PhotoList()
+                    .tabItem {
+                        Label("Photos", systemImage: "photo.stack")
+                    }
+                    .tag(Tab.photos)
+                ProfileView()
+                    .tabItem {
+                        Label("Profile", systemImage: "person.crop.circle")
+                    }
+                    .tag(Tab.profile)
+                
+            }
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                .environmentObject(PublicPostsViewModel())
+        }
     }
 }
